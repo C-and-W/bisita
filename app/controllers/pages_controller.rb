1145aa9539a264661f420ac3f7856ea512a49ced
jacqhernandez <jm.hernandez96@yahoo.com>
2016-03-07 19:59:04 -0300

    started search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,7 @@
 class PagesController < ApplicationController
   before_action :set_query, only: [:search, :results]
 
-  def try
-    @query = params['query']
-    if @query != nil
-      @search = Church.search do 
-      fulltext @query do 
-        highlight :background
-        highlight :artistic_values
-        end
-        order_by(:score, :asc)
-      end
-      @churches = @search.results
-    elsif @query == ""
-      @churches = Church.all
-    end
+def try
     @search = Church.search do 
       fulltext params['query'] do 
         highlight :background
@@ -25,6 +12,18 @@ class PagesController < ApplicationController
 
     @churches = @search.results
     #@hits = search.hits
+  end
+
+  def search
+    if params['query'] != nil
+      if @churches.count == 0
+        @message = "Sorry, we couldn't find what you searched for."
+      else
+        redirect_to results_path(:query => params['query'])
+      end
+    else
+      render :search
+    end
   end
 
   def login
@@ -39,17 +38,18 @@ class PagesController < ApplicationController
   def contact
   end
 
-  def search
-    if params['query'] != nil 
-      if @churches.count != 0
-        redirect_to results_path(:query => @query)
-      else
-        @message = "Sorry, we couldn't find what you searched for."
-      end
-    else
-      render :search
-    end
-  end
+  # def search
+  #   if params['query'] != nil 
+  #     if @churches.count != 0
+  #       redirect_to results_path(:query => @query)
+  #     else
+  #       @message = "Sorry, we couldn't find what you searched for."
+  #     end
+  #   else
+  #     render :search
+  #   end
+  # end
+
 
   def results
   end
@@ -59,11 +59,25 @@ class PagesController < ApplicationController
 
   private
     def set_query
-      @query = params['query']
-      if @query != nil
-        @churches = Church.where(['name LIKE ? OR location LIKE ? OR background LIKE ? OR architecture LIKE ? OR make LIKE ? OR id IN (SELECT church_id FROM artistic_values WHERE description LIKE ?) OR id IN (SELECT church_id FROM facts WHERE description LIKE ?) OR id IN (SELECT church_id FROM timeline_points WHERE description LIKE ?)',"%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%"])
-      elsif @query == ""
-        @churches = Church.all
+      if params['query'] != nil
+        @search = Church.search do 
+          fulltext params['query'] do 
+            highlight :background
+            highlight :artistic_values
+          end
+          order_by(:score, :asc)
+        end
+        @churches = @search.results
       end
     end
+
+  # private
+  #   def set_query
+  #     @query = params['query']
+  #     if @query != nil
+  #       @churches = Church.where(['name LIKE ? OR location LIKE ? OR background LIKE ? OR architecture LIKE ? OR make LIKE ? OR id IN (SELECT church_id FROM artistic_values WHERE description LIKE ?) OR id IN (SELECT church_id FROM facts WHERE description LIKE ?) OR id IN (SELECT church_id FROM timeline_points WHERE description LIKE ?)',"%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%"])
+  #     elsif @query == ""
+  #       @churches = Church.all
+  #     end
+  #   end
 end
