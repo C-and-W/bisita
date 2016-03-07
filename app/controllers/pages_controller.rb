64da7ba15fc1493d33fb5a6f2d08e40cf8c9eadc
jacqhernandez <jm.hernandez96@yahoo.com>
2016-03-07 19:59:46 -0300

    started search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,6 @@
 class PagesController < ApplicationController
   before_action :set_query, only: [:search, :results]
 
-def try
-    @search = Church.search do 
-      fulltext params['query'] do 
-        highlight :background
-        highlight :artistic_values
-      end
-      order_by(:score, :asc)
-    end
-
-    @churches = @search.results
-    #@hits = search.hits
-  end
-
   def search
     if params['query'] != nil
       if @churches.count == 0
@@ -38,19 +25,6 @@ def try
   def contact
   end
 
-  # def search
-  #   if params['query'] != nil 
-  #     if @churches.count != 0
-  #       redirect_to results_path(:query => @query)
-  #     else
-  #       @message = "Sorry, we couldn't find what you searched for."
-  #     end
-  #   else
-  #     render :search
-  #   end
-  # end
-
-
   def results
   end
 
@@ -71,13 +45,4 @@ def try
       end
     end
 
-  # private
-  #   def set_query
-  #     @query = params['query']
-  #     if @query != nil
-  #       @churches = Church.where(['name LIKE ? OR location LIKE ? OR background LIKE ? OR architecture LIKE ? OR make LIKE ? OR id IN (SELECT church_id FROM artistic_values WHERE description LIKE ?) OR id IN (SELECT church_id FROM facts WHERE description LIKE ?) OR id IN (SELECT church_id FROM timeline_points WHERE description LIKE ?)',"%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%","%#{@query}%"])
-  #     elsif @query == ""
-  #       @churches = Church.all
-  #     end
-  #   end
 end
